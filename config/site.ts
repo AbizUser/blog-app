5bab8b8b9b36b304a806edfeead013707bc5fa3b
AbizUser <aman.biz.t23@gmail.com>
2024-11-23 17:00:00 +0900

    add mainpage

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig = {
+  name: "Post Writer",
+  description: "",
+  url: "https://localhost:3000",
+  ogImage: "",
+  links: {
+    x: "https://www.udemy.com/course/learn-basics-of-linux/?couponCode=BFCPSALE24JP",
+    github: "https://github.com/AbizUser",
+  }
+};
