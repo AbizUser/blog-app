7799b9bef238a158b8c55c2e4e05db55b00b8aff
AbizUser <aman.biz.t23@gmail.com>
2024-11-23 23:28:10 +0900

    add footer

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
 import MainNav from "@/components/ui/main-nav";
+import SiteFooter from "@/components/ui/site-footer";
 import { marketingConfig } from "@/config/marketing";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
@@ -28,6 +29,7 @@ export default function MarketiongLayout({
         </div>
       </header>
       <main>{children}</main>
+      <SiteFooter/>
     </div>
   );
 }
