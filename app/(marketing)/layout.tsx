62a4ebdb95ee84e51f71fc6cd6108bca18b87ee5
AbizUser <aman.biz.t23@gmail.com>
2024-11-23 17:12:41 +0900

    add header

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
 import MainNav from "@/components/ui/main-nav";
+import { marketingConfig } from "@/config/marketing";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
@@ -12,7 +13,7 @@ export default function MarketiongLayout({
     <div>
       <header className="container z-40 bg-background">
         <div className="h-20 py-6 flex items-center justify-between">
-          <MainNav/>
+          <MainNav items={marketingConfig.mainNav}/>
           <nav>
             <Link 
               href={"/login"}
